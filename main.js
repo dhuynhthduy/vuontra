9cb76b35ea4e8f91961a6fe7a16a7af3f11f0a37
dhuynhthduy <dhuynhthduy@gmail.com>
2022-05-04 12:05:11 +0700

    update

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 
-function collapse(e){
+function collapse(e) {
     e.classList.toggle("collapse-Show");
 }
+
+var navbarMenu = document.getElementById("navbar")
+// var showNavbar = document.querySelector("#header show")
+console.log(navbarMenu)
+
+var aItems = document.querySelectorAll('#header ul li a')
+
+
+for (var i = 0; i < aItems.length; i++) {
+    var aItem = aItems[i];
+    aItem.onclick = function () {
+        console.log(this)
+        navbarMenu.style.display = null;
+    }
+}
+
+
